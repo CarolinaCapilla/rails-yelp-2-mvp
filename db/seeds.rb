297ee8f1cc0774a3715d55e46c89c4230871beef
CarolinaCapilla <carolina.capilla@gmail.com>
2021-01-26 19:05:38 +0000

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+10.times do
+  restaurants = [
+    Restaurant.create(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.street_address,
+      category: %w[chinese italian japanese french belgian].sample,
+      phone_number: Faker::PhoneNumber.cell_phone_in_e164
+    )
+  ]
+  restaurants.each do |restaurant|
+    puts "Created #{restaurant.id}"
+  end
+end
+
+# first_restaurant = { name: Faker::Restaurant.name, address: Faker::Address.street_address,
+#                      category: %w[chinese italian japanese french belgian].sample,
+#                      phone_number: Faker::PhoneNumber.cell_phone_in_e164 }
+
+# second_restaurant = { name: Faker::Restaurant.name, address: Faker::Address.street_address,
+#                       category: %w[chinese italian japanese french belgian].sample,
+#                       phone_number: Faker::PhoneNumber.cell_phone_in_e164 }
+
+# [first_restaurant, second_restaurant].each do |restaurant|
+#   restaurant = Restaurant.create!(restaurant)
+#   puts "Created #{restaurant.name}"
+# end
+
+puts 'Finished'
